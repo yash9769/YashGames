0902e42d57707153189273c6058b0388089ec44d
yash9769 <yashraj9769@gmail.com>
2026-04-02 23:26:00 +0530

    Fix console errors (audio 403, and meta tag deprecation)

--- src/lib/audio.js
+++ src/lib/audio.js
@@ -2,10 +2,10 @@
 // Basic audio utility to play sound effects.
 
 const sounds = {
-  pop: 'https://cdn.pixabay.com/download/audio/2022/03/15/audio_2443af12a2.mp3?filename=pop-39222.mp3',
-  success: 'https://cdn.pixabay.com/download/audio/2021/08/04/audio_0625c1539c.mp3?filename=success-1-6297.mp3',
-  error: 'https://cdn.pixabay.com/download/audio/2022/03/24/audio_33bc4e7fe4.mp3?filename=error-126627.mp3',
-  win: 'https://cdn.pixabay.com/download/audio/2022/10/25/audio_51bb6cbf32.mp3?filename=kids-greeting-win-100262.mp3'
+  pop: '',
+  success: '',
+  error: '',
+  win: ''
 }
 
 const audioElements = {}
